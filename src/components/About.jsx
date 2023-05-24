f25a5aa33a20241f7647bfc22f659c0efc793272
halan10 <64984593+halan10@users.noreply.github.com>
2023-05-24 13:18:37 -0700

    Create React project

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+export function About() {
+  return (
+    <>
+      <h1>TASK MANAGMENT</h1>
+      <h2>About Task Management App</h2>
+      <p>
+        Stay organized and efficient with a powerful task management platform
+        Plan, manage and track all your team's tasks in one flexible software.
+      </p>
+    </>
+  );
+}
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
+import logo from "../../../src/images/icon6.jpg";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to="/">
+          <img src={logo} style={{ height: 50, width: 50 }} />
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          <NavLink to="/todo" activeStyle>
+            ToDo
+          </NavLink>
+          <NavLink to="/contact-us" activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to="/sing-up" activeStyle>
+            Sing Up
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to="singin">Sing In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
